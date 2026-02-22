e642190fe099f71cfcafa2bf7dbc5df04fa12577
Steve Lombardi <smlombardi@gmail.com>
2026-02-22 12:36:53 -0500

    feat: integrate Tailwind CSS into the project
    - Added Tailwind CSS and its Vite plugin to dependencies in package.json.
    - Imported Tailwind CSS in the main CSS file.
    - Updated main.ts to include the main CSS file.
    - Configured Vite to use the Tailwind CSS plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import VueRouter from 'vue-router/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,6 +14,7 @@ export default defineConfig({
     }),
     vue(),
     vueDevTools(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
